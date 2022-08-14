ca73e4416733fa2750ce0b7fa08da9ce2da04e49
AG11G <gontaandrei11@gmail.com>
2022-08-14 12:07:32 -0500

    New changes

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json", "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@HR-6",
+        tags = "@HR-8",
         // Test UI regression test cases. -> @regression and @ui
         // run only those feature files that has either @HR-5 or @HR-6 tag.
         // @HR-5 and @HR-6 -> run only those features files that has both @HR-5 and @HR-6 tags.
